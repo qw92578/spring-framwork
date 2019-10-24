83e6c389986c49a6a17dd25e22c4d6d2e534e80f
fanggq <qw92578@gmail.com>
2019-10-24 14:15:40 +0800

    environment config

---
+++ src/main/java/com/qw/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.qw.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Package: com.qw.controller
+ * @ClassName: TestController
+ * @Author: fgq
+ * @Description:
+ * @Date: 2019/10/24 13:54
+ */
+@Controller
+public class TestController {
+}
---
+++ src/main/java/com/qw/dao/TestDao.java
@@ -0,0 +1,11 @@
+package com.qw.dao;
+
+/**
+ * @Package: com.qw.dao
+ * @ClassName: TestDao
+ * @Author: fgq
+ * @Description:
+ * @Date: 2019/10/24 14:14
+ */
+public class TestDao {
+}
